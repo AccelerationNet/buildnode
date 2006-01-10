dd6728d40c300b09ce7af3699800eb1fd13b7d3f
russ <russ@acceleration.net>
2006-01-10 13:28:10 -0500

    added js packager to lisp
    darcs-hash:20060110182810-03e4f-d6d3125e88161b3cd8da8c50e7b9f70ae34c3fbd.gz

---
+++ src/js-packager.lisp
@@ -0,0 +1,116 @@
+(in-package :net.acceleration.javascript)
+
+(defstruct js-dependency-graph
+  (hash (make-hash-table :test #'equal)))
+
+(defstruct js-graph-node
+  (url)
+  (dependency-list))
+
+(defvar *global-js-dependency-graph* (make-js-depenecy-graph))
+
+(defclass js-collector ()
+  ((url-hash
+	 :initarg :url-hash
+	 :accessor url-hash
+	 :initform (make-hash-table :test #'equal)
+	 :documentation "a hash table that stores what urls are needed")
+	))
+
+(defun find-graph-node (key js-dependency-graph)
+  (gethash key (js-dependency-graph-hash js-dependency-graph) ))
+
+(defun (setf find-graph-node) (val key js-dependency-graph)
+  (setf (gethash key (js-dependency-graph-hash js-dependency-graph)) val))
+
+
+(defun make-js-collector ()
+  "small quicky alias for make-instance 'js-collector)"
+  (make-instance 'js-collector))
+
+(defmethod js-list ((js js-collector))
+  (let ((result '()))
+	 (maphash
+	  
+	  (lambda (key val)
+		 (mapc
+		  
+		  (lambda (elem)
+			 (unless (member elem result :test #'string-equal)
+				(nconc result (list elem))))
+		  
+		  (get-dependency-list key))
+		 (unless (member val result :test #'string-equal)
+			(nconc result (list key))))
+	  
+	  (url-hash js))
+	 result))
+
+(defun get-dependency-list (key)
+  "gets a list of all the files required for proper execution  of a javascript file
+that is designated by the key (either a keyword in the *global-js-dependency-graph* or a url)"
+  (let ((return-list '()))
+	 (kmrcl:awhen (find-graph-node key *global-js-dependency-graph*)
+		(mapc (lambda (elem)
+				  (nconc  
+					(if (stringp elem)
+						 (list elem)
+						 (get-dependency-list elem))
+					return-list))
+				(js-graph-node-dependency-list it)))
+	 return-list))
+
+(defun def-js-file (js-name url &key depends-on)
+  "defines a named (using a keyword) javascript file and its dependencies"
+	 (print js::*global-js-dependency-graph*)
+	 (setf (find-graph-node js-name js::*global-js-dependency-graph*) (make-js-graph-node :url url :dependency-list depends-on))
+	 (setf (find-graph-node url js::*global-js-dependency-graph*) (make-js-graph-node :url url :dependency-list depends-on)))
+
+(defun def-anon-js-file (js-name url &key depends-on)
+  "defines an js file and its dependencies using only a url"
+ 
+	 (setf (find-graph-node url *global-js-dependency-graph*) (make-js-graph-node :url url :dependency-list depends-on)))
+
+(defun js-defined-p (js-key-or-url)
+  "has a js-file been defined either with or without a name"
+ 
+	
+	 (nth-value 1 (gethash js-key-or-url *global-js-dependency-graph*)))
+
+(defun use-js-file (js-collector url-or-key &key depends-on)
+  (unless (js-defined-p url-or-key)
+	 (if (stringp url-or-key)
+		  (def-anon-js-file url-or-key :depends-on depends-on)
+		  (error (format nil "js-file with name ~s is not defined" url-or-key))))
+  (if (stringp url-or-key)
+		(setf (gethash url-or-key (url-hash js-collector)) T)
+		(setf (gethash 
+				 (car (gethash url-or-key *global-js-dependency-graph*))
+				 (url-hash js-collector)) T)))
+
+(defmacro with-javascript-collector ((variable &optional (js-collector (make-js-collector))) &body body)
+  `(let ((,variable ,js-collector)
+			(use-js-file))
+	 (declare (special ,variable))
+	 (declare (special use-js-file))
+	 (setf (symbol-function 'use-js-file)
+	  (lambda (url-or-key &key depends-on )
+		 (funcall #'net.acceleration.javascript::use-js-file ,variable url-or-key depends-on))
+	 ,@body)))
+
+
+
+
+
+
+
+(def-js-file :JsHelper "/jscontrols/jshelper.js" :depends-on '() )
+(def-js-file :Control "/jscontrols/Control.js" :depends-on '(:JsHelper) )
+(def-js-file :Data "/jscontrols/Data.js" :depends-on '(:JsHelper) )
+(def-js-file :Http "/jscontrols/Http.js" :depends-on '(:JsHelper) )
+(def-js-file :Grid "/jscontrols/Grid/grid.js" :depends-on '(:JsHelper :Data :Http :Control) )
+(def-js-file :Collections "/jscontrols/Collections.js" :depends-on '(:JsHelper))
+(def-js-file :Hashtable "/jscontrols/Collections.Hashtable.js" :depends-on '(:JsHelper :Collections))
+(def-js-file :Behaviour "/jscontrols/Behaviour.js" :depends-on '(:Hashtable))
+
+
