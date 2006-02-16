298e53eb9aa33126703c87c16c83ebfa888d909c
Nathan Bird <nathan@acceleration.net>
2006-02-16 17:23:14 -0500

    indentation
    darcs-hash:20060216222314-783f4-ba196922c62d8fcb547361ca7cd25c32417cd53c.gz

--- src/buildnode.lisp
+++ src/buildnode.lisp
@@ -3,17 +3,17 @@
 (defun append-nodes (to-location &rest chillins)
   "appends a bunch of dom-nodes (chillins) to the location specified"
   (let ((children (kmrcl:flatten chillins))
-		  (doc (if (subtypep (type-of to-location) 'rune-dom::document)
-					  to-location
-					  (dom:owner-document to-location))))
-	 (iterate (for child in children)
-				 (when child
-					(dom:append-child
-					 to-location
-					 (if (stringp child)
-						  (dom:create-text-node doc child)
-						  child))))
-	 to-location))
+	(doc (if (subtypep (type-of to-location) 'rune-dom::document)
+		 to-location
+		 (dom:owner-document to-location))))
+    (iterate (for child in children)
+	     (when child
+	       (dom:append-child
+		to-location
+		(if (stringp child)
+		    (dom:create-text-node doc child)
+		    child))))
+    to-location))
 
 (defvar *namespace-prefix-map* '(("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul" . "xul")
 				 ("http://www.w3.org/1999/xhtml" . "xhtml")))
@@ -46,31 +46,31 @@ If the tagname does not contain a prefix, then one is added based on the namespa
 (defun write-document-to-character-stream (document char-stream)
   "writes a cxml:dom document to a character stream"
   (let ((buf (flex:with-output-to-sequence (stream)
-					(write-document-to-octet-stream document stream))))
+	       (write-document-to-octet-stream document stream))))
 	 
-	 (flex:with-input-from-sequence (input buf)
-		;;echo the flexi stream to output
-		(with-open-stream (echo (make-echo-stream (flex:make-flexi-stream input)
-																char-stream))
-		  (loop for line = (read-line echo nil)
-				  while line)))))
+    (flex:with-input-from-sequence (input buf)
+      ;;echo the flexi stream to output
+      (with-open-stream (echo (make-echo-stream (flex:make-flexi-stream input)
+						char-stream))
+	(loop for line = (read-line echo nil)
+	      while line)))))
 
 
 (defun write-document-to-octet-stream (document octet-stream)
-    "writes a cxml:dom document to a character stream"
+  "writes a cxml:dom document to a character stream"
   (dom:map-document (cxml:make-namespace-normalizer (cxml:make-octet-stream-sink
-																	  octet-stream
-																	  :canonical nil
-																	  :indentation 2))
-						  document
-						  :include-doctype nil
-						  ))
+						     octet-stream
+						     :canonical nil
+						     :indentation 2))
+		    document
+		    :include-doctype nil
+		    ))
 
 (defun write-document (document &optional (out-stream *standard-output*))
   "Write the document to the designated out-stream, or *standard-ouput* by default."
   (case (stream-element-type out-stream) 
-	 ('character (write-document-to-character-stream document out-stream))
-	 ('octet (write-document-to-octet-stream document out-stream))))
+    ('character (write-document-to-character-stream document out-stream))
+    ('octet (write-document-to-octet-stream document out-stream))))
 
 
 (defmacro with-document (&body chillins)
@@ -80,9 +80,9 @@ a document is necessary to create dom nodes and the document the nodes end up on
 must be the document on which they were created.  At the end of the form, the
 complete document is returned"
   `(let ((*document*  (cxml-dom:create-document)))
-	 (declare (special *document*))
-	 (append-nodes *document* ,@chillins)
-	 *document*))
+    (declare (special *document*))
+    (append-nodes *document* ,@chillins)
+    *document*))
 
 (defmacro with-document-to-file (filename &body chillins)
   "Creates a document block with-document upon which to add the chillins
